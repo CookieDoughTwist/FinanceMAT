cbc3ebc5f15b25c3af701bad95f65f3f5f3d83a9
CookieDoughTwist <andrew01810@gmail.com>
2018-01-12 22:31:55 -0500

    Plots updated.

--- Binance/TickerTracker.m
+++ Binance/TickerTracker.m
@@ -2,13 +2,16 @@ classdef TickerTracker < handle
     properties
         name;
         quantity;
-        price_data;
+        price_data;        
         
         falling;
         
         window_ns;
         price_averages;
         price_dif_averages;
+        
+        price_1st_dif_averages;
+        
     end
     
     methods
@@ -27,16 +30,22 @@ classdef TickerTracker < handle
             for ii = 1:length(self.window_ns)
                 self.price_averages{ii} = [];
                 self.price_dif_averages{ii} = [];
-            end            
+                self.price_1st_dif_averages{ii} = [];
+            end
         end
 
         function update(self,in_data)
             self.price_data(end+1) = in_data;
-            price_dif = diff(self.price_data);
+            price_dif = diff(self.price_data);            
             for ii = 1:length(self.window_ns)
                 n = self.window_ns(ii);
                 self.price_averages{ii}(end+1) = mean_n(self.price_data,n);
                 self.price_dif_averages{ii}(end+1) = mean_n(price_dif,n);
+            end
+            price_1st_dif = diff(self.price_averages{1});
+            for ii = 1:length(self.window_ns)
+                n = self.window_ns(ii);
+                self.price_1st_dif_averages{ii}(end+1) = mean_n(price_1st_dif,n);
             end
         end
         
@@ -49,90 +58,35 @@ classdef TickerTracker < handle
         end
         
         function plot(self)
-            figure();
-            hold on;
-            title([self.name ' Prices']);
-            xlabel('t');
-            ylabel('btc');
-            plot(self.price_data);
-            for ii = 1:length(self.window_ns)
-                plot(self.price_averages{ii});
-            end
-            legend(['Price';split(num2str(self.window_ns))]);
-            
-            figure();
-            hold on;
-            title([self.name ' Der']);
-            xlabel('t');
-            ylabel('btc');
-            for ii = 1:length(self.window_ns)
-                plot(self.price_averages{ii});
-            end
-            legend(split(num2str(self.window_ns)));
+%             figure();
+%             title([self.name ' Price']);
+%             xlabel('Intervals (10s)');
+%             ylabel('BTC');
+%             plot(self.price_data);
+            self.plot_averages(self.price_averages,'Price Averages');
+            self.plot_averages(self.price_dif_averages,'Dif Averages');
+            self.plot_averages(self.price_1st_dif_averages,'First window dif averages');
         end
         
-        function plot_stats(self)
-            fig = figure();
+        function plot_averages(self,avg_cells,title_str)
+            figure();
             hold on;
-%             set(fig,'Color','k');
-%             setAxesColor(gca,'w','k');
-            title([self.name ' Prices']);
-            xlabel('t');
-            ylabel('btc');
-            p = self.price_data;            
-            plot(p,'r');
-            plot(movmean(p,[6 0]),'g');
-            plot(movmean(p,[60 0]),'b');
-            legend({'Price','1min','10min'});
-            
-            fig = figure();
-            hold on;
-            title([self.name ' Derivatives']);
-            xlabel('t');
-            ylabel('btc');
-%             d = diff(self.price_data);            
-            d = diff(movmean(p,[6 0]));            
-%             plot(d,'r');
-            plot(d,'r');
-            plot(movmean(d,[6 0]),'g');
-            plot(movmean(d,[60 0]),'b');
-            legend({'Price','1min','10min'});
-%             legend({'1min','10min'});
-
-            fig = figure();
-            hold on;
-            title([self.name ' Derivatives']);
-            xlabel('t');
-            ylabel('btc');
-%             d = diff(self.price_data);            
-            dd = diff(movmean(p,[60 0]));            
-%             plot(d,'r');
-            plot(dd,'r');
-            plot(movmean(dd,[6 0]),'g');
-            plot(movmean(dd,[60 0]),'b');
-            legend({'Price','1min','10min'});
-%             legend({'1min','10min'});
-
-            fig = figure();
-            hold on;
-            title([self.name ' Derivatives']);
-            xlabel('t');
-            ylabel('btc');
-%             d = diff(self.price_data);            
-            dd = diff(diff(movmean(p,[6 0])));            
-%             plot(d,'r');
-            plot(dd,'r');
-            plot(movmean(dd,[6 0]),'g');
-            plot(movmean(dd,[60 0]),'b');
-            legend({'Price','1min','10min'});
-%             legend({'1min','10min'});
+            title([self.name ' ' title_str]);
+            xlabel('Intervals (10s)');
+            ylabel('BTC');
+            plot(self.price_data);
+            for ii = 1:length(self.window_ns)
+                plot(avg_cells{ii});
+            end
+            legend(['Price'; split(num2str(self.window_ns))]);
         end
     end
 end
 
 function avg = mean_n(arr,n)
     if n > length(arr)
-        avg = mean(arr);
+%         avg = mean(arr);
+        avg = NaN;
     else
         s_idx = length(arr)-n+1;
         avg = mean(arr(s_idx:end));
